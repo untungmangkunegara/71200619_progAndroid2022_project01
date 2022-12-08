a8df14c0a32a252fc768f40980bcc3621fda5d9a
untungmangkunegara <untung.mangkunegara@ti.ukdw.ac.id>
2022-12-08 15:54:08 +0700

    commit

--- app/src/main/java/com/example/a71200619_project_01/ThirdActivity.kt
+++ app/src/main/java/com/example/a71200619_project_01/ThirdActivity.kt
@@ -3,15 +3,32 @@ package com.example.a71200619_project_01
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Toast
+import android.widget.*
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        supportActionBar?.hide()
+
+        val KotaAsal = intent.getStringExtra("iniKotaAsal").toString()
+        val KotaTujuan = intent.getStringExtra("iniKotaTujuan").toString()
+        val TanggalBerangkat = intent.getStringExtra("tanggalBerangkat").toString()
+        val JamBerangkat = intent.getStringExtra("iniJamKeberangkatan").toString()
+        val KelasTiket = intent.getStringExtra("iniKelasTiket").toString()
+
+        val tvKotaAsal = findViewById<TextView>(R.id.tvSumKotaAsal)
+        val tvKotaTujuan = findViewById<TextView>(R.id.tvSumKotaTujuan)
+        val tvTanggalBerangkat = findViewById<TextView>(R.id.tvSumTanggalBerangkat)
+        val tvJamBerangkat = findViewById<TextView>(R.id.tvSumJamBerangkat)
+        val tvKelasTiket = findViewById<TextView>(R.id.tvSumKelasTiket)
+
+        tvKotaAsal.text = "Kota Asal: $KotaAsal"
+        tvKotaTujuan.text = "Kota Tujuan: $KotaTujuan"
+        tvJamBerangkat.text = "Jam Berangkat: $JamBerangkat"
+        tvKelasTiket.text = "Kelas Tiket: $KelasTiket"
+
 
 
     }
